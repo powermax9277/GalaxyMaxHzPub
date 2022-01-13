6478a06fa391b21d9d45ea27be35ec49ed2291cf
tribalfs <tribalfs@gmail.com>
2022-01-13 08:14:35 +0800

    fixed dialog crash on rotation

--- app/src/main/java/com/tribalfs/gmh/dialogs/InfoDialog.kt
+++ app/src/main/java/com/tribalfs/gmh/dialogs/InfoDialog.kt
@@ -4,12 +4,34 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
 
-class InfoDialog(private val tit: Int, private val msg: Int): RoundedDialogFragment(){
+class InfoDialog(): RoundedDialogFragment(){
+
+    private var msg: Int? = null
+    private var title: Int? = null
+
+    companion object {
+        fun newInstance(title: Int, msg: Int): InfoDialog {
+            val f = InfoDialog()
+            val args = Bundle()
+            args.putInt("msg", msg)
+            args.putInt("title", title)
+            f.arguments = args
+            return f
+        }
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        msg = arguments?.getInt("msg")
+        title = arguments?.getInt("title")
+    }
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         isCancelable = false
         return AlertDialog.Builder(context).apply {
-            setTitle(tit)
-            setMessage(msg)
+            setTitle(title as Int)
+            setMessage(msg as Int)
             setPositiveButton(android.R.string.ok) { dialog, _ ->
                dialog.dismiss()
             }
--- app/src/main/java/com/tribalfs/gmh/dialogs/RoundedDialogFragment.kt
+++ app/src/main/java/com/tribalfs/gmh/dialogs/RoundedDialogFragment.kt
@@ -1,26 +1,25 @@
 package com.tribalfs.gmh.dialogs
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.Window
+import androidx.appcompat.content.res.AppCompatResources.getDrawable
 import androidx.fragment.app.DialogFragment
 import com.tribalfs.gmh.R
 
 open class RoundedDialogFragment : DialogFragment() {
-    @SuppressLint("UseCompatLoadingForDrawables")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.dialog_rounded, container, false)
         // Set transparent background and no title
         if (dialog != null && dialog!!.window != null) {
-            dialog!!.window!!.setBackgroundDrawable(resources.getDrawable(R.drawable.rounded_edge, null))
+            dialog!!.window!!.setBackgroundDrawable(getDrawable(requireContext(), R.drawable.rounded_edge))
             dialog!!.window!!.requestFeature(Window.FEATURE_NO_TITLE)
         }
+        val view = inflater.inflate(R.layout.dialog_rounded, container, false)
         return view
     }
 }
