2572b1b4af78c879e8bfead8c2e4465872dac6f8
tribalfs <tribalfs@gmail.com>
2022-02-21 06:16:31 +0800

    Bug fixes

--- app/src/main/java/com/tribalfs/gmh/profiles/ProfilesObj.kt
+++ app/src/main/java/com/tribalfs/gmh/profiles/ProfilesObj.kt
@@ -8,12 +8,11 @@ object ProfilesObj: Serializable {
     @Volatile
     var refreshRateModeMap = mutableMapOf<String, List<Map<String, ResolutionDetails>>>()
 
-    //TODO test @get:Synchronized @set:Synchronized
     var adaptiveModelsObj = mutableListOf<String>()
 
     @Volatile
     var isProfilesLoaded: Boolean = false
 
-    @Volatile //TODO test not necessary @get:Synchronized @set:Synchronized
+    @Volatile
     var loadComplete: Boolean = false// means all modes completely loaded
 }
